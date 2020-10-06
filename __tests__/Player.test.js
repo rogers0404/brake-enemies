306b8a70561732d2a0562354ecd82f9f54c09ee4
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-10-06 18:48:51 -0400

    Built the Player Object and test for it

---
+++ __tests__/Player.test.js
@@ -0,0 +1,22 @@
+const Potion = require('../lib/Potion');
+
+const Player = require('../lib/Player');
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
